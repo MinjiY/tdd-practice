6837274e338c912d29c87ae0b91f68f10d09f8d7
MinjiY <upi907@naver.com>
2025-07-08 16:18:29 +0900

    [Feature] 유저 포인트 충전시 PointHistory가 기록되어야한다.: Green - 유저 포인트 충전 비즈니스 로직에 PointHistory 저장 추가

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -4,6 +4,7 @@ import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.exception.ResourceNotFoundException;
 import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
@@ -33,6 +34,7 @@ public class PointService {
      * 특정 유저의 포인트를 충전하는 기능
      */
     public UserPoint charge(long id, long amount) {
+        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
         return userPointTable.insertOrUpdate(id, amount);
     }
 }
