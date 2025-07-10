a00b096d13fad57bafd2d2dc11e484600c7c723c
MinjiY <upi907@naver.com>
2025-07-11 08:55:58 +0900

    [Feature] 유저의 포인트 조회 성공 케이스, Response 검증

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,15 +22,22 @@ class PointControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserPointTable userPointTable;
+
 
     @Test
     @DisplayName("유저의 포인트 조회 API - 성공")
     void chargePointApi_success() throws Exception {
         // given
         long userId = 1L;
-        mockMvc.perform(get("/point/{id}", userId))
-                .andExpect(status().isOk());
-    }
+        long amount = 1000L;
+        userPointTable.insertOrUpdate(userId, amount);
 
+        mockMvc.perform(get("/point/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.point").value(amount));
+    }
 
 }
