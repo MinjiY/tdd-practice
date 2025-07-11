b49e295e61e115e67302ea1db950f04be82f2e63
MinjiY <upi907@naver.com>
2025-07-11 09:29:30 +0900

    [Feature] 유저의 포인트 충전/사용 기록 조회 API

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,6 +29,8 @@ class PointControllerTest {
     @Autowired
     private UserPointTable userPointTable;
 
+    @Autowired
+    private PointHistoryTable pointHistoryTable;
 
     @Test
     @DisplayName("유저의 포인트 조회 API - 성공")
@@ -78,4 +81,32 @@ class PointControllerTest {
                 .andExpect(jsonPath("$.point").value(initialAmount - useAmount));
     }
 
+    @Test
+    @DisplayName("유저의 포인트 충전/사용 기록 조회 API")
+    void getPointHistoryApi_success() throws Exception {
+        // given
+        long userId = 1L;
+        long chargeAmount = 500L;
+        long useAmount = 300L;
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/point/{id}/charge", userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(chargeAmount)))
+            .andExpect(status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/point/{id}/use", userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(useAmount)))
+            .andExpect(status().isOk());
+
+        // when & then
+        mockMvc.perform(get("/point/{id}/histories", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].userId").value(userId))
+                .andExpect(jsonPath("$[0].amount").value(chargeAmount))
+                .andExpect(jsonPath("$[0].type").value("CHARGE"))
+                .andExpect(jsonPath("$[1].userId").value(userId))
+                .andExpect(jsonPath("$[1].amount").value(useAmount))
+                .andExpect(jsonPath("$[1].type").value("USE"));
+    }
 }
